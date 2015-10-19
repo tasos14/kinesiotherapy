9ead9979c30c2dc32fc7e62d18f54350bfeb5571
Tasos Theodoridis <tasos14tasos@gmail.com>
2015-10-19 21:46:36 +0300

    Added map

---
+++ js/map.js
@@ -0,0 +1,20 @@
+function init_map() {
+  var var_location = new google.maps.LatLng(40.731152, 22.915024);
+
+  var var_mapoptions = {
+    center: var_location,
+    zoom: 15
+  };
+
+  var var_marker = new google.maps.Marker({
+    position: var_location,
+    map: var_map,
+		title:"Venice"});
+
+  var var_map = new google.maps.Map(document.getElementById("map-container"),var_mapoptions);
+
+  var_marker.setMap(var_map);
+
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);
