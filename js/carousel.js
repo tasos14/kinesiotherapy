d4cf85a7cbcf881308e23289831d518c925cfb04
Tasos Theodoridis <tasos14tasos@gmail.com>
2015-10-31 21:41:24 +0200

    Added partners section

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#carousel").owlCarousel({
+    navigation : false, // Show next and prev buttons
+    rewindNav :true, 
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true,
+    autoPlay: 3000
+  });
+});
